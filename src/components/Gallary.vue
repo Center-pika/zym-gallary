9f8d108259f6e4f96ca882104a9c13c03ccf6233
onefolder <ooonefolder@gmail.com>
2021-08-11 22:34:52 +0800

    section initial

--- src/components/Gallary.vue
+++ src/components/Gallary.vue
@@ -1,32 +1,27 @@
 <template>
   <div class="gallary" :style="this.heightStyle">
     <waterfall
-      ref="waterfall"
-      :imgsArr="imgs"
-      @scrollReachBottom="loadImg"
-      :imgWidth="280"
-      :maxCols="6"
+      :col="col"
+      :width="itemWidth"
+      :gutterWidth="gutterWidth"
+      :data="imgs"
+      @loadmore="loadmore"
     >
-      <!-- <div class="img-info" slot-scope="props">
-        <p class="info">{{ props.value.info }}</p>
-      </div> -->
-      <div slot="waterfall-over">
-        <h5>Photoed By SNH-48 张月铭</h5>
-        <p>Presented By 中心皮卡丘</p>
-      </div>
+      <template>
+        <div class="cell-item" v-for="item in imgs" v-bind:key="item.name">
+          <img v-if="item" :lazy-src="item['src']" alt="加载错误" />
+        </div>
+      </template>
     </waterfall>
   </div>
 </template>
 
 <script>
-import waterfall from "vue-waterfall-easy";
 import axios from "axios";
 
 export default {
   name: "Gallary",
-  components: {
-    waterfall,
-  },
+  components: {},
   props: {
     collapsed: Boolean,
     base: String,
@@ -38,6 +33,7 @@ export default {
       group: 0,
       group_size: 15,
       height: window.innerHeight,
+      col: 5
     };
   },
   methods: {
@@ -63,6 +59,9 @@ export default {
       );
       this.group++;
     },
+    loadmore(index) {
+      console.log(index);
+    },
     // clickHandler(event) {
     //   event.preventDefault();
     // },
@@ -83,6 +82,12 @@ export default {
       return {
         height: this.collapsed ? "100%" : "90%",
       };
+    },
+    itemWidth() {
+      return 138 * 0.5 * (document.documentElement.clientWidth / 375);
+    },
+    gutterWidth() {
+      return 9 * 0.5 * (document.documentElement.clientWidth / 375);
     },
   },
 };
